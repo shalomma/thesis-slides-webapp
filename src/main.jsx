f095e47b22afa402e55acea42b2aa25278fa9003
Maayan Shalom <sha.maayan@gmail.com>
2025-06-29 05:46:04 +0300

    <MathJaxContext config={config}>       <App />

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { MathJaxContext } from 'better-react-mathjax'
+
+const config = {
+  loader: { load: ["input/tex", "output/chtml"] },
+};
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <MathJaxContext config={config}>
+      <App />
+    </MathJaxContext>
   </StrictMode>,
 )
